8873566827fab60f31df6fdfc44dd2ac7c96fcbb
galuh wikri <galuhwikri05@gmail.com>
2024-10-29 00:18:02 +0700

    Menyelesaikan Model

---
+++ app/Models/Post.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Models;
+use Illuminate\Support\Arr;
+use phpDocumentor\Reflection\Types\Static_;
+
+class post
+{
+    public static function all  ()
+    {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel 1',
+                'title' => 'Judul artikel 1',
+                'author' => 'Galuh wikri',
+                'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industry,s standard dummy text ever since the 1500s, when an
+             unknown printer took a galley of type and scrambled it to make a type specimen book.
+             It has survived not only five centuries, but also the leap into electronic typesetting,
+             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+             Aldus PageMaker including versions of Lorem Ipsum'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel 2',
+                'title' => 'Judul artikel 2',
+                'author' => 'Galuh wikri',
+                'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industry,s standard dummy text ever since the 1500s, when an
+             unknown printer took a galley of type and scrambled it to make a type specimen book.
+             It has survived not only five centuries, but also the leap into electronic typesetting,
+             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+             Aldus PageMaker including versions of Lorem Ipsum'
+            ]
+        ];
+    }
+    public static function find($slug)
+    {
+        return Arr::first(Static::all(), fn($post) => $post['slug'] === $slug);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -12,69 +13,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'posts', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel 1',
-            'title' => 'Judul artikel 1',
-            'author' => 'Galuh wikri',
-            'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry,s standard dummy text ever since the 1500s, when an
-             unknown printer took a galley of type and scrambled it to make a type specimen book.
-             It has survived not only five centuries, but also the leap into electronic typesetting,
-             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-             Aldus PageMaker including versions of Lorem Ipsum'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel 2',
-            'title' => 'Judul artikel 2',
-            'author' => 'Galuh wikri',
-            'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry,s standard dummy text ever since the 1500s, when an
-             unknown printer took a galley of type and scrambled it to make a type specimen book.
-             It has survived not only five centuries, but also the leap into electronic typesetting,
-             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-             Aldus PageMaker including versions of Lorem Ipsum'
-        ]
-    ]]);
+    return view('posts', ['title' => 'posts', 'posts' => Post::all() ]);
 });
 
 route::get('posts/{slug}', function ($slug) {
-    $post = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel 1',
-            'title' => 'Judul artikel 1',
-            'author' => 'Galuh wikri',
-            'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry,s standard dummy text ever since the 1500s, when an
-             unknown printer took a galley of type and scrambled it to make a type specimen book.
-             It has survived not only five centuries, but also the leap into electronic typesetting,
-             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-             Aldus PageMaker including versions of Lorem Ipsum'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel 1',
-            'title' => 'Judul artikel 2',
-            'author' => 'Galuh wikri',
-            'body' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry,s standard dummy text ever since the 1500s, when an
-             unknown printer took a galley of type and scrambled it to make a type specimen book.
-             It has survived not only five centuries, but also the leap into electronic typesetting,
-             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-             sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-             Aldus PageMaker including versions of Lorem Ipsum'
-        ]
-    ];
-
-    $post = arr::first($post, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
     return view('post', ['title' => 'Single post' , 'post' => $post ]);
 });
 
